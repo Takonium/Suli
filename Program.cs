524d14e48e27f6a75b46698049df4580607d705c
Takács Attila <58575227+Takonium@users.noreply.github.com>
2022-09-13 12:10:45 +0200

    Feladat #1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 40; i++)
+            {
+                list.Add(rnd.Next(-100, 101));
+                Console.Write($"{list[i]}; ");
+            }
+
+            int paros = 0;
+            for (int i = 0; i < 40; i++) if (list[i] % 2 == 0) paros++;
+            Console.WriteLine($"\n{paros} páros szám van");
+            Console.ReadKey();
+        }
+    }
+}
